0290d4b53af35819b4e5735b5eecce052bd084fc
FedericoVelez25 <federico.velezferro@gmail.com>
2022-07-11 10:55:05 +0200

    new data added

--- Challenge1.js
+++ Challenge1.js
@@ -1,11 +1,20 @@
-//Mark
-let marksWeight = 78;
-let marksHeight = 1.69;
+//Mark 1
+//let marksWeight = 78;
+//let marksHeight = 1.69;
 
-//John
-let johnWeight = 92;
-let johnHeight = 1.95;
+//John 1
+//let johnWeight = 92;
+//let johnHeight = 1.95;
 
+//Mark 2
+let marksWeight = 95;
+let marksHeight = 1.88;
+
+//John 2
+let johnWeight = 85;
+let johnHeight = 1.76;
+
+//BMI Formulas
 let marksBmi = 78 / 1.69 ** 2;
 let johnsBmi = 92 / (1.95 * 1.95);
 
