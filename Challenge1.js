c244fd011e5354f99721f1d973b56a8b20e5fa0f
FedericoVelez25 <federico.velezferro@gmail.com>
2022-07-11 12:19:07 +0200

    challenge 2 done

--- Challenge1.js
+++ Challenge1.js
@@ -18,8 +18,14 @@ let johnHeight = 1.76;
 let marksBmi = 78 / 1.69 ** 2;
 let johnsBmi = 92 / (1.95 * 1.95);
 
-console.log(marksBmi);
-console.log(johnsBmi);
+//console.log(marksBmi);
+//console.log(johnsBmi);
 
 let markHigherBmi = marksBmi > johnsBmi;
-console.log(markHigherBmi);
+//console.log(markHigherBmi);
+
+if (marksBmi > johnsBmi) {
+  console.log(`Mark's BMI (${marksBmi}) is higher than John's!`);
+} else {
+  console.log(`John's BMI (${johnsBmi}) is higher than Mark's!`);
+}
